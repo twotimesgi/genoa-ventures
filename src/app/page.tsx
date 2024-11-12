0702194d277578a2bca8ac8c0c5bf980e77a4ef1
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-11-12 18:36:02 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { Clients } from "@/components/clients";
 import { ContactUs } from "@/components/contact-us";
 import { Hero } from "@/components/hero";
-import { Mentors } from "@/components/mentors";
 import { Pricing } from "@/components/pricing";
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
    <main>
     <Hero/>
     <Clients/>
-    {/* <Mentors/> */}
     <Pricing/>
     <ContactUs/>
      </main>
